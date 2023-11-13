d3965fe2a5bfbb86a4f3dae5c5b651fb1d4174a5
fl3my <rossflem@icloud.com>
2023-11-13 18:30:44 +0000

    Added string builder for creating hex string

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 class Dec2Hex
 {
@@ -11,16 +12,16 @@ class Dec2Hex
 		int decimalNum = Integer.parseInt(args[0]);
 		
 		// Initialize a char array with all hexadecimal values
-		char hexChars[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		char[] hexChars = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		
 		int remainder;
 		int num = decimalNum;
 		
-		// Initialize a string to store the hexidecimal output
-		String hexadecimal = "";
-		
 		// Print a start message
 		System.out.println("Converting the Decimal Value " + num + " to Hex...");
+		
+		// Create an empty string builder object
+		StringBuilder hexadecimal = new StringBuilder();
 
 		// While the number is not equal to 0
 		while(num != 0)
@@ -28,15 +29,15 @@ class Dec2Hex
 			// Get the remainder when the number is divided by 16
 			remainder = num % 16;
 
-			// Append the string with approriate string character
-			hexadecimal = hexChars[remainder] + hexadecimal;
+			// Insert the hex character to the first place in the string
+			hexadecimal.insert(0, hexChars[remainder]);
 
 			// Divide the number by 16
 			num = num / 16;
 		}
 		
 		// Print the resulting hexadecimal string
-		System.out.println("Hexadecimal representation is: " + hexadecimal);
+		System.out.println("Hexadecimal representation is: " + hexadecimal.toString());
 	}
 }
 
