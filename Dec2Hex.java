f44f6353954067d5fafff4dfd77c2b2dcd36ab0a
fl3my <rossflem@icloud.com>
2023-11-13 20:11:54 +0000

    Seperate to function and add unit tests

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,30 +5,13 @@ import java.io.IOException;
 
 class Dec2Hex
 {
-	public static String convert(int decimal) {
-		return "test";
-	}
-
-	public static void main(String args[]) throws IOException
-	{
-		// Read in the logging properties file
-		LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
-		
-		// Create a logger
-		Logger logger = Logger.getLogger(Dec2Hex.class.getName());
-
-		// Get the number from the first command line argument
-		int decimalNum = Integer.parseInt(args[0]);
-		
+	public static String convert(int decimalNum) {	
 		// Initialize a char array with all hexadecimal values
 		char[] hexChars = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		
 		int remainder;
 		int num = decimalNum;
 		
-		// Print a start message using a logger
-		logger.info(String.format("Converting the Decimal Value %d to Hex...", num));
-
 		// Create an empty string builder object
 		StringBuilder hexadecimal = new StringBuilder();
 
@@ -44,7 +27,26 @@ class Dec2Hex
 			// Divide the number by 16
 			num = num / 16;
 		}
+
+		return hexadecimal.toString();
+	}
+
+	public static void main(String args[]) throws IOException
+	{
+		// Read in the logging properties file
+		LogManager.getLogManager().readConfiguration(new FileInputStream("logging.properties"));
 		
+		// Create a logger
+		Logger logger = Logger.getLogger(Dec2Hex.class.getName());
+
+		// Get the number from the first command line argument
+		int decimalNum = Integer.parseInt(args[0]);
+	
+		// Print a start message using a logger
+		logger.info(String.format("Converting the Decimal Value %d to Hex...", decimalNum));
+		
+		String hexadecimal = Dec2Hex.convert(decimalNum);
+
 		// Print the resulting hexadecimal string
 		logger.info(String.format("Hexadecimal representation is: %s", hexadecimal.toString()));
 	}
--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -12,9 +12,39 @@ public class Dec2HexTest {
 	public void setUp() {
 		dec2hex = new Dec2Hex();
 	}
-
+	
+	// Test basic cases
 	@Test
 	public void testConvertValidNumberToHex() {
-		assertEquals("2a", dec2hex.convert(42));
+		assertEquals("2A", dec2hex.convert(42));
 	}
+	
+	@Test
+	public void testConvertZeroToHex() {
+		assertEquals("0", dec2hex.convert(0));
+	}
+	
+	// Test edge cases
+	@Test
+	public void testConvertOneToHex() {
+		assertEquals("1", dec2hex.convert(1));
+	}
+
+	@Test
+	public void testConvertMaxEdgeCaseToHex() {
+		assertEquals("FFF", dec2hex.convert(4095));
+	}
+
+	@Test
+	public void testConvertLargeNumberToHex() {
+		assertEquals("FFFF", dec2hex.convert(65535));
+	}
+
+	// Test negative numbers
+	@Test
+	public void testConvertNegativeNumberToHex() {
+		assertEquals("-2A", dec2hex.convert(-42));
+	}
+
+
 }
